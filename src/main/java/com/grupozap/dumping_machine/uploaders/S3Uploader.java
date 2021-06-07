b44e6b6d9788f11767053a3a6157fa10c2379d61
André Casado <andre.castano@grupozap.com>
2021-06-07 13:34:34 -0300

    Adding canned acl "BucketOwnerFullControl" to uploaded S3 files. (#23)
    Allow insertion of path in S3BucketName inside yaml config file.

--- src/main/java/com/grupozap/dumping_machine/uploaders/S3Uploader.java
+++ src/main/java/com/grupozap/dumping_machine/uploaders/S3Uploader.java
@@ -3,7 +3,11 @@ package com.grupozap.dumping_machine.uploaders;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.grupozap.dumping_machine.partitioners.HourlyBasedPartitioner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
@@ -11,6 +15,8 @@ public class S3Uploader implements Uploader {
     private final String bucketName;
     private final String bucketRegion;
     private final static String TYPE = "s3a";
+
+    private final Logger logger = LoggerFactory.getLogger(HourlyBasedPartitioner.class);
 
     public S3Uploader(String bucketName, String bucketRegion) {
         this.bucketName = bucketName;
@@ -22,8 +28,18 @@ public class S3Uploader implements Uploader {
                 .withRegion(this.bucketRegion)
                 .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
+        String bucketName = this.bucketName;
+        String bucketPath = "";
+        if (bucketName.indexOf('/') >= 0) {
+            bucketPath = bucketName.substring(bucketName.indexOf('/') + 1);
+            bucketName = bucketName.substring(0, bucketName.indexOf('/'));
+        }
+        if (bucketPath.matches(".*[^/]$")) {
+            bucketPath = bucketPath + "/";
+        }
+        logger.info("putObject BucketName:[" + bucketName + "],  bucketPath: [" + bucketPath + "],  remotePath: [" + remotePath + "], filename: [" + filename + "]");
 
-        s3Client.putObject(new PutObjectRequest(this.bucketName, remotePath, new File(filename)));
+        s3Client.putObject(new PutObjectRequest(bucketName, bucketPath + remotePath, new File(filename)).withCannedAcl(CannedAccessControlList.BucketOwnerFullControl));
     }
     public String getServerPath() {
         return TYPE + "://" + bucketName;
